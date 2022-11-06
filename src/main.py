bdaa0075b5ebd17d9925d2cd9f8b23ea6d2b907a
Dev-Nergis <78343032+Dev-Nergis@users.noreply.github.com>
2022-11-06 16:50:29 +0900

    asdasdas

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+os.system("git clone https://github.com/Grasscutters/Grasscutter")
+os.chdir("Grasscutter")
+os.system("del /Q start.cmd start_config.cmd")
+
+jar = os.listdir("Grasscutter")
+jar = jar.index("*.jar")
+print(jar)
+
+if jar == None:
+    os.system(".\gradlew.bat")
+    os.system(".\gradlew jar")
+else:
+    pass
+
+with open("start.cmd", "w") as file:
+    file.write(f"java -jar {jar}")
+
+os.system(".\start.cmd")
