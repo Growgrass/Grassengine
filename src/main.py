d72d6f51fdc83e7b6fc167743c2bdb0168804de5
Dev-Nergis <78343032+Dev-Nergis@users.noreply.github.com>
2022-11-06 21:29:54 +0900

    up

--- src/main.py
+++ src/main.py
@@ -1,25 +1,52 @@
-import os, re, shutil
-from pathlib import Path
+import os, re, typer
 
-os.system("git clone https://github.com/Grasscutters/Grasscutter")
-os.system("git clone https://github.com/tamilpp25/Grasscutter_Resources")
+app = typer.Typer(name="Grassengine")
 
-os.chdir(os.getcwd() + "\Grasscutter")
-os.system("del /Q start.cmd start_config.cmd")
-os.system("rmdir /Q resources")
+@app.command(name="main")
+def main():
+    print("!Grassengine!")
+    print("""
 
-os.system(".\gradlew.bat")
-os.system(".\gradlew jar")
+    Grasscutter 설치기
 
-jar = os.listdir()
-jar = str(jar)
-jar = str(re.findall(r'grasscutter.*?.jar', jar)).replace(r"['", "").replace(r"']", "")
+    만든이: Growgrass
+    버전: 1.0.0
+    """)
 
-with open("start.cmd", "w") as file:
-    file.write(f"java -jar {jar}")
+@app.command(name="setup", help="기본 설치")
+def setup(option: str = typer.Argument(default=None, help="option: Grasscutter, Resources")):  # type: ignore
 
-os.chdir("../")
-os.chdir(os.getcwd() + "\Grasscutter_Resources")
-shutil.copytree("Resources", "../Grasscutter/resources")
+    if option == "Grasscutter":
+        os.system("git clone https://github.com/Grasscutters/Grasscutter")
 
-os.system(".\start.cmd")
+        os.chdir(os.getcwd() + "\Grasscutter") # type: ignore
+        os.system("del /Q start.cmd start_config.cmd")
+        os.system(".\gradlew.bat") # type: ignore
+        os.system(".\gradlew jar") # type: ignore
+    elif option == "Resources":
+        os.system("rmdir /Q resources")
+        os.chdir("../")
+        os.system("git clone https://github.com/tamilpp25/Grasscutter_Resources")
+        print("""
+        Grasscutter_Resources 폴더 안에있는 Resources 폴더를 Grasscutter 폴더로 복사한다음 이름을 resources로 바꾸어 주세요.
+        """)
+    else:
+        print("python main.py setup --help")
+
+@app.command(name="start", help="Grasscutter 시작")
+def start():
+    os.chdir(os.getcwd() + "\Grasscutter") # type: ignore
+
+    jar = os.listdir()
+    jar = str(jar)
+    jar = str(re.findall(r'grasscutter.*?.jar', jar)).replace(r"['", "").replace(r"']", "")
+
+    print("Grasscutter를 시작합니다...")
+
+    if jar == "[]":
+        print("python main.py setup --Grasscutter 을 먼저 실행해 주세요.")
+    else:
+        os.system(f"java -jar {jar}")
+
+if __name__ == "__main__":
+    app()
